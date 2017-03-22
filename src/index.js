9741c955749dd7aeef8d672c13ab6406c8e0066e
Adam Siwiec <brickmaster01@gmail.com>
2017-03-22 12:02:12 -0500

    bug fixes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ require('jquery');
 require('./main.css');
 require('fullpage.js');
 
-
+$(document).hide();
 $(document).ready(function() {
+
     $('#fullpage').fullpage({
       sectionsColor: ['#eee', '#eee', '#eee'],
       navigation: true
     });
+    $(document).show();
 });
